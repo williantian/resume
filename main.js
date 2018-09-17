5fc4e110f15d3b762a3a13716f9abd1d5b78df3e
willian <williantian1995@gmail.com>
2018-09-17 17:29:28 +0800

    加入简单非线性跳转动画

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+function animate(time) {
+    requestAnimationFrame(animate);
+    TWEEN.update(time); //tween函数  采取的库
+}
+requestAnimationFrame(animate);//如何运行由上面的函数决定
+
 setTimeout(function(){
     siteWlecome.classList.remove('active')
 },1000)
@@ -29,19 +35,30 @@ for(let i=0; i<aTags.length; i++){
         let rect = element.getBoundingClientRect()
         let top = element.offsetTop
 
-        let n = 25 //一共滚动多少次
-        let duration = 500 / n //多少时间动一次  一次持续多长时间
+        //let n = 25 //一共滚动多少次
+        //let duration = 500 / n //多少时间动一次  一次持续多长时间
         let currentTop = window.scrollY
         let targetTop = top - 80
-        let distance = (targetTop - currentTop) / n//每一次滚动多长距离
-        let i = 0
-        let id = setInterval(()=> {
-            if(i===n){
-                window.clearInterval(id)
-                return
-            }
-            i = i + 1
-            window.scrollTo(0, currentTop + distance * i)
-        },duration)
+        //let distance = (targetTop - currentTop) / n//每一次滚动多长距离
+        //let i = 0
+        //let id = setInterval(()=> {
+            //if(i===n){
+                //window.clearInterval(id)
+                //return
+            //}
+            //i = i + 1
+            //window.scrollTo(0, currentTop + distance * i)
+        //},duration)
+        var coords = { y: currentTop }; 
+        let s = targetTop - currentTop
+        let t = Math.abs((s/100)*500)//Math.abs取绝对值
+        if(t>1000){t=1000}//最大1秒运行完成
+        var tween = new TWEEN.Tween(coords)     
+            .to({ y: targetTop }, t)                  
+            .easing(TWEEN.Easing.Quadratic.InOut)
+            .onUpdate(function(){
+              window.scrollTo(0, coords.y)//coords.y 是函数算出来的
+            })
+            .start(); 
     }
 }
